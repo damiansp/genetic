314b3150ccec2751663710ed8193da69700ab0b0
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2017-02-21 09:21:14 -0800

    finished string_find.py; abstracted out into genetic.py and guess_password.py

---
+++ 01_hello_world/genetic.py
@@ -0,0 +1,44 @@
+import random
+
+# _functions are protected
+def _generate_parent(length, gene_set):
+    genes = []
+
+    while len(genes) < length:
+        sample_size = min(length - len(genes), len(gene_set))
+        genes.extend(random.sample(gene_set, sample_size))
+
+    return ''.join(genes)
+
+def _mutate(parent, gene_set):
+    index = random.randrange(0, len(parent))
+    child_genes = list(parent)
+    new_gene, alternate = random.sample(gene_set, 2)
+    child_genes[index] = (alternate if new_gene == child_genes[index]
+                          else new_gene)
+    return ''.join(child_genes)
+
+
+def get_best(get_fitness, target_len, optimal_fitness, gene_set, display):
+    random.seed()
+    best_parent = _generate_parent(target_len, gene_set)
+    best_fitness = get_fitness(best_parent)
+    display(best_parent)
+
+    if best_fitness >= optimal_fitness:
+        return best_parent
+
+    while True:
+        child = _mutate(best_parent, gene_set)
+        child_fitness = get_fitness(child)
+
+        if best_fitness >= child_fitness:
+            continue
+
+        display(child)
+
+        if child_fitness >= optimal_fitness:
+            return child
+
+        best_fitness = child_fitness
+        best_parent = child
--- 01_hello_world/string_find.py
+++ 01_hello_world/string_find.py
@@ -1,3 +1,4 @@
+import datetime
 import random
 
 gene_set = 'abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ.?!'
@@ -11,3 +12,41 @@ def generate_parent(length):
         genes.extend(random.sample(gene_set, sample_size))
 
     return ''.join(genes)
+
+def get_fitness(guess):
+    return sum(
+        1 for expected, actual in zip(target, guess) if expected == actual)
+
+def mutate(parent):
+    index = random.randrange(0, len(parent))
+    child_genes = list(parent)
+    new_gene, alternate = random.sample(gene_set, 2)
+    child_genes[index] = (alternate if new_gene == child_genes[index]
+                          else new_gene)
+    return ''.join(child_genes)
+
+def display(guess):
+    time_diff = datetime.datetime.now() - start_time
+    fitness = get_fitness(guess)
+    print('{0}\t{1}\t{2}'.format(guess, fitness, str(time_diff)))
+
+random.seed()
+start_time = datetime.datetime.now()
+best_parent = generate_parent(len(target))
+best_fitness = get_fitness(best_parent)
+display(best_parent)
+
+while True:
+    child = mutate(best_parent)
+    child_fitness = get_fitness(child)
+
+    if best_fitness >= child_fitness:
+        continue
+
+    display(child)
+
+    if child_fitness >= len(best_parent):
+        break
+
+    best_fitness = child_fitness
+    best_parent = child
