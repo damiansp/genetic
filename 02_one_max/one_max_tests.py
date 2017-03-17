c2ab0a3812dd50819961e6c6e6229ed1e2609402
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2017-03-17 01:32:57 -0400

    init one max

--- 02_one_max/one_max_tests.py
+++ 02_one_max/one_max_tests.py
@@ -5,7 +5,36 @@ import unittest
 sys.path.append('../01_hello_world')
 import genetic
 
+def get_fitness(genes):
+    return genes.count(1)
+
+def display(candidate, start_time):
+    time_diff = datetime.datetime.now() - start_time
+    print('{0}...{1}\t{2:3.2f}\t{3}'.format(
+        ''.join(map(str, candidate.Genes[:15])),
+        ''.join(map(str, candidate.Genes[-15:])),
+        candidate.Fitness,
+        str(time_diff)))
+
 class OneMaxTests(unittest.TestCase):
     def test(self, length = 100):
         gene_set = [0, 1]
+        start_time = datetime.datetime.now()
+
+        def fn_display(candidate):
+            display(candidate, start_time)
+
+        def fn_get_fitness(genes):
+            return get_fitness(genes)
+
+        optimal_fitness = length
+        best = genetic.get_best(
+            fn_get_fitness, length, optimal_fitness, gene_set, fn_display)
+        self.assertEqual(best.Fitness, optimal_Fitness)
         
+    def test_benchmark(self):
+        genetic.Benchmark.run(lambda: self.test(4000))
+        
+if __name__ == '__main__':
+    unittest.main()
+
