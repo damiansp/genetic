db9bb1e9a6cefafce9b1df596681f520a2303f00
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-10-02 13:25:51 -0700

    init Neuron and InputNeuron

--- neural_net/network.py
+++ neural_net/network.py
@@ -12,9 +12,18 @@ def main():
     print(data_set)
     print(data_set.data.head())
     [X_train, y_train], [X_test, y_test] = data_set.split_data()
-    features = list(X_train)
-    print(features)
-    network = Network(features)
+    feature_names = list(X_train)
+    print(feature_names)
+    network = Network(feature_names)
+    network.show_nodes()
+
+
+def identity(x):
+    return x
+
+
+def relu(x):
+    return x if x > 0 else 0
 
 
 class DataSet:
@@ -62,14 +71,39 @@ class DataSet:
 
 class Network:
     def __init__(self, feature_names, n_inputs=1, n_outputs=1):
-        self.features = features
+        # feature_names should include all possible features, though not all
+        # networks will necessarily use all of them
+        self.feature_names = feature_names
         self.n_inputs = n_inputs
         self.n_outputs = n_outputs
-        self.inputs = []
+        self.input_nodes = [InputNeuron(name) for name in self.feature_names]
+
+    def show_nodes(self):
+        for node in self.input_nodes:
+            print(node)
+        
+        
+class Neuron:
+    id_iter = itertools.count()
+    
+    def __init__(self, activation=identity):
+        self.idn = next(Neuron.id_iter)
+        self.activation = activation
+        self.inputs = [1] # bias
+
+    def __str__(self):
+        return f'Neuron {self.idn}'
 
 
-def relu(x):
-    return x if x > 0 else 0
+class InputNeuron(Neuron):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def __str__(self):
+        return f'Input{super().__str__()}: {self.name}'
+
+    
 
 
 
